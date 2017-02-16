60d134d825a369474c62650d41379a585b338122
Francisco Balam <fco.balam@hotmail.com>
2017-02-16 11:37:43 -0600

    Devise url format without namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   	  post '/finalize_rent', to: 'movies#finalize_rent'
   	end
   end
-  devise_for :users
+  devise_for :users, path: "", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'new' }
   resources :users
   root to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
